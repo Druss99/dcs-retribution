a06e9d0ec73c03404ceb0744b84a62a4dcbe9215
Raffson <Raffson@users.noreply.github.com>
2024-12-23 18:06:11 +0100

    Plan more ANTI-SHIP against ship-groups with air-defences

--- game/commander/tasks/primitive/antiship.py
+++ game/commander/tasks/primitive/antiship.py
@@ -7,12 +7,16 @@ from game.commander.missionproposals import EscortType
 from game.commander.tasks.packageplanningtask import PackagePlanningTask
 from game.commander.theaterstate import TheaterState
 from game.theater.theatergroundobject import NavalGroundObject
+from game.utils import meters
 
 
 @dataclass
 class PlanAntiShip(PackagePlanningTask[NavalGroundObject]):
     def preconditions_met(self, state: TheaterState) -> bool:
-        if self.target not in state.threatening_air_defenses:
+        if (
+            self.target not in state.threatening_air_defenses
+            and not self.target.is_naval_control_point
+        ):
             return False
         if not self.target_area_preconditions_met(state, ignore_iads=True):
             return False
@@ -24,5 +28,11 @@ class PlanAntiShip(PackagePlanningTask[NavalGroundObject]):
 
     def propose_flights(self) -> None:
         size = self.get_flight_size()
+        if self.target.max_detection_range() > meters(0):
+            size = 4  # attempt to saturate ship's air-defences
+            self.propose_flight(FlightType.ANTISHIP, size)
+            self.propose_flight(FlightType.ESCORT, 2, EscortType.AirToAir)
         self.propose_flight(FlightType.ANTISHIP, size)
         self.propose_flight(FlightType.ESCORT, 2, EscortType.AirToAir)
+        if self.target.max_detection_range() > meters(0):
+            self.propose_flight(FlightType.SEAD, 2, EscortType.Sead)
